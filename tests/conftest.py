0824f2903fb73562295cb9f60ae61f894b3e9272
orbxball <junyouliu9@gmail.com>
2021-02-10 12:53:43 -0500

    Initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,67 @@
+import pytest
+from brownie import config
+from brownie import Contract
+
+
+@pytest.fixture
+def gov(accounts):
+    yield accounts[0]
+
+
+@pytest.fixture
+def rewards(accounts):
+    yield accounts[1]
+
+
+@pytest.fixture
+def guardian(accounts):
+    yield accounts[2]
+
+
+@pytest.fixture
+def management(accounts):
+    yield accounts[3]
+
+
+@pytest.fixture
+def strategist(accounts):
+    yield accounts[4]
+
+
+@pytest.fixture
+def keeper(accounts):
+    yield accounts[5]
+
+
+@pytest.fixture
+def token():
+    token_address = "0x6b175474e89094c44da98b954eedeac495271d0f"  # this should be the address of the ERC-20 used by the strategy/vault
+    yield Contract(token_address)
+
+
+@pytest.fixture
+def amount(accounts, token):
+    amount = 10_000 * 10 ** token.decimals()
+    # In order to get some funds for the token you are about to use,
+    # it impersonate an exchange address to use it's funds.
+    reserve = accounts.at("0xd551234ae421e3bcba99a0da6d736074f22192ff", force=True)
+    token.transfer(accounts[0], amount, {"from": reserve})
+    yield amount
+
+
+@pytest.fixture
+def vault(pm, gov, rewards, guardian, management, token):
+    Vault = pm(config["dependencies"][0]).Vault
+    vault = guardian.deploy(Vault)
+    vault.initialize(token, gov, rewards, "", "", guardian)
+    vault.setDepositLimit(2 ** 256 - 1, {"from": gov})
+    vault.setManagement(management, {"from": gov})
+    yield vault
+
+
+@pytest.fixture
+def strategy(strategist, keeper, vault, Strategy, gov):
+    strategy = strategist.deploy(Strategy, vault)
+    strategy.setKeeper(keeper)
+    vault.addStrategy(strategy, 10_000, 0, 1_000, {"from": gov})
+    yield strategy
---
+++ tests/test_operation.py
@@ -0,0 +1,32 @@
+from brownie import Contract
+
+
+def test_operation(accounts, token, vault, strategy, strategist, amount):
+    # Deposit to the vault
+    token.approve(vault.address, amount, {"from": accounts[0]})
+    vault.deposit(amount, {"from": accounts[0]})
+    assert token.balanceOf(vault.address) == amount
+
+    # harvest
+    strategy.harvest()
+    assert token.balanceOf(strategy.address) == amount
+
+    # tend()
+    strategy.tend()
+
+    # withdrawal
+    vault.withdraw({"from": accounts[0]})
+    assert token.balanceOf(accounts[0]) != 0
+
+
+def test_emergency_exit(accounts, token, vault, strategy, strategist, amount):
+    # Deposit to the vault
+    token.approve(vault.address, amount, {"from": accounts[0]})
+    vault.deposit(amount, {"from": accounts[0]})
+    strategy.harvest()
+    assert token.balanceOf(strategy.address) == amount
+
+    # set emergency and exit
+    strategy.setEmergencyExit()
+    strategy.harvest()
+    assert token.balanceOf(strategy.address) < amount
