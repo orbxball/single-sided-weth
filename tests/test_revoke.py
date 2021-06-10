2eec053ee5b9728ec2feb1a79ff58f0436511ae9
orbxball <orbxball@protonmail.com>
2021-06-10 16:28:17 -0400

    feat: update revoke test

--- tests/test_revoke.py
+++ tests/test_revoke.py
@@ -1,9 +1,15 @@
 import pytest
+from brownie import chain
 
 
-def test_revoke(gov, vault, strategy, token):
-    vault.revokeStrategy(strategy, {"from": gov})
+def test_revoke(gov, vault, strategy, token, whale, amount):
+    scale = 10 ** token.decimals()
+    # Deposit to the vault
+    token.approve(vault, 2 ** 256 - 1, {"from": whale})
+    vault.deposit(amount, {"from": whale})
+    assert token.balanceOf(vault.address) == amount
     strategy.harvest({"from": gov})
 
-    assert vault.totalDebt() == 0
-    assert vault.totalAssets() == token.balanceOf(vault)
+    vault.revokeStrategy(strategy, {"from": gov})
+    strategy.harvest({"from": gov})
+    print(f"strategy info: {vault.strategies(strategy).dict()}")
